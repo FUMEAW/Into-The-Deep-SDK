4e760ddec4c773745401396fa381314210675892
akhil <akhil.anguluri@gmail.com>
2024-10-18 19:26:24 -0400

    Arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -19,6 +19,7 @@ public class MecanumTeleOp extends LinearOpMode {
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("BottomLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("TopRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("BottomRightMotor");
+        DcMotor testMotor = hardwareMap.dcMotor.get("ArmMotor");
 
 //        double integralSum = 0;
 //        double Kp = 0;
@@ -68,6 +69,90 @@ public class MecanumTeleOp extends LinearOpMode {
                 backLeftMotor.setPower(backLeftPower);
                 frontRightMotor.setPower(frontRightPower);
                 backRightMotor.setPower(backRightPower);
+
+                DcMotor armLeft = hardwareMap.dcMotor.get("armLeft");
+                DcMotor armRight = hardwareMap.dcMotor.get("armRight");
+                armRight = hardwareMap.dcMotor.get("armRight");
+
+                // Reverse backwards arm motor
+                armRight.setDirection(DcMotor.Direction.REVERSE);
+
+                // Set arm encoders to 0
+                armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+                // Set arm run mode
+                armLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                armRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                // Zero Power Behavior
+                armLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                armRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+                // Setup Telemetry, will not clear after cycle, setup reusable items for output
+                telemetry.setAutoClear(false);
+                Telemetry.Item armLeftPosition = telemetry.addData("Left Arm Position", armLeft.getCurrentPosition());
+                Telemetry.Item armRightPosition = telemetry.addData("Right Arm Position", armRight.getCurrentPosition());
+
+
+                //code before waitForStart is run when Init button is pressed
+                while(!isStarted){
+                    //print encoder counts to telemetry while we manually move the arm
+                    armLeftPosition.setValue(armLeft.getCurrentPosition());
+                    telemetry.update();
+                }
+
+                //code after waitForStart is run when the start button is pressed
+
+
+                int armTarget = 0;
+                double armSpeed = 0;
+                String armCurrentDirection = 'up';
+
+                while (isOpModeActive()) {
+
+                    /**
+                     * BEGIN ARM LIFT
+
+                     * Gamepad 1 btn A - arm lift up
+                     * Gamepad 1 btn B - arm lift down
+                     *
+                     * !!! concerned about reliability of encoder count as it doesn't seem to be tracking accurately
+                     **/
+
+                    if (gamepad1.a){ // Arm UP
+                        armTarget = 200;
+                        armSpeed = 0.98;
+                        armCurrentDirection = 'up';
+
+                        armLeft.setPower(armSpeed);
+                        armLeft.setTargetPosition(armTarget);
+
+                    } else if (gamepad1.b){ // Arm DOWN
+                        armTarget = 0;
+                        armSpeed = -0.1;  // From my research, negative is ignore, so I don't understand why this *seemed* to work
+                        armCurrentDirection = 'down';
+
+                        armLeft.setPower(armSpeed);
+                        armLeft.setTargetPosition(armTarget);
+                    }
+
+                    // Remove Power from the Arm Motor if motor is close to 0 position, arm should drop
+                    if ( armCurrentDirection === 'down' && ( armLeft.getTargetPosition() < 5 || armRight.getTargetPosition() < 5) ){
+                        armSpeed = 0;
+                        armLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                        armRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    }
+
+                    /** END ARM LIFT **/
+
+
+                    idle();
+
+                    // Arm Lift Telemetry
+                    if( armLeft.isBusy() ){
+                        armLeftPosition.setValue(armLeft.getCurrentPosition());
+                        telemetry.update();
 
 
             }
